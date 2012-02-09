100a0c41171645b1218cd4facf1bc5c3507bdd05
Matt Oskamp <7mao@queensu.ca>
2012-02-09 17:15:59 -0500

    Added the Species.cs class

---
+++ CircleOfLife/CircleOfLife/Species.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CircleOfLife
+{
+    class Species
+    {
+        // Properties of Species
+
+        // User defined attributes
+        string Name;
+        float Population;
+        Texture2D Graphic;
+        float Size;
+        float Sight;
+        float Speed;
+        // Not sure if we will have these as base stats or just part of perks
+        float Attack;
+        float Defence;
+
+        // Non-user defined attributes
+        float Hunger;
+        float Thirst;
+        float Energy;
+        // add perks
+
+        public void Initialize()
+        {
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+        }
+    }
+}
