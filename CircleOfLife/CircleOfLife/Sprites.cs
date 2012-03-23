f4daf96b74bd121d4d61cd939145055658f2d97a
unknown <Ozzy@Ozzy-PC.(none)>
2012-03-23 12:15:07 -0400

    merging

---
+++ CircleOfLife/CircleOfLife/Sprites.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace CircleOfLife
+{
+    static class Sprites
+    {
+        public static Rectangle chaosBoundary = new Rectangle(0, 0, 1000, 1000);
+        public static Rectangle herbivore = new Rectangle(0, 1200, 100, 100);
+        public static Rectangle carnivore = new Rectangle(0, 1300, 100, 100);
+        public static Rectangle hudBackground = new Rectangle(0, 1000, 50, 150);
+        public static Rectangle torchButton = new Rectangle(50, 1000, 150, 150);
+    }
+}
